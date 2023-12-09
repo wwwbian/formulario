004da51f784a67f19c91fec06a7c79225b3b69d5
wwwbian <bdiaz@escuelasproa.edu.ar>
2023-12-09 10:32:34 -0300

    HICE QUE SE VEA EL EJS

--- servidor.js
+++ servidor.js
@@ -1,7 +1,12 @@
 const express= require('express');
 const app= express();
 const mongoose = require('mongoose');
+const path = require('path')
+const ejs = require('ejs')
 const port= 3000
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 //Conectar MongoDB
 mongoose.connect('mongodb+srv://bdiaz:unodostres@clusterr.xffnu0h.mongodb.net/',{
@@ -14,6 +19,10 @@ mongoose.connect('mongodb+srv://bdiaz:unodostres@clusterr.xffnu0h.mongodb.net/',
 .catch((err) =>{
     console.error('error al conectar el servidor :'+err) 
 });
+app.get('/', (req, res) => {
+    res.render('index', { message: 'Formulario' });
+});
+
   
 app.listen(port, () => {
     console.log('hola, el servidor anda');
