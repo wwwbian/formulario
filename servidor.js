bb6a5415ef92e171c83acb70df84254cfc355d17
wwwbian <bdiaz@escuelasproa.edu.ar>
2023-12-09 13:18:13 -0300

    sss

--- servidor.js
+++ servidor.js
@@ -5,6 +5,7 @@ const path = require('path')
 const ejs = require('ejs')
 const port= 3000
 const userRoutes= require('./routes/user')
+
 app.use('/api',userRoutes)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
